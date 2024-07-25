d0f6594081de91019fc72749c9aa51bb17eb1e35
Jana <webdev7@iascertification.com>
2024-07-25 17:43:04 +0530

    deploy

--- backend-app/app.js
+++ backend-app/app.js
@@ -9,7 +9,7 @@ const connectDB = require('./db');
 require('dotenv').config();
 
 const corsOptions = {
-  origin: ['http://localhost:3000', 'https://github.com'],
+  origin: ['http://localhost:3000', 'https://sjjana77.github.io'],
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   optionsSuccessStatus: 200
 };
@@ -19,11 +19,11 @@ app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/react_task/users', userRoutes);
-app.use('/react_task/books', bookRoutes);
-app.use('/react_task/transactions', transactionRoutes);
-app.use('/react_task/books', bookRoutes);
-app.use('/react_task/transactions', transactionRoutes);
+app.use('/library_managemeny_system/users', userRoutes);
+app.use('/library_managemeny_system/books', bookRoutes);
+app.use('/library_managemeny_system/transactions', transactionRoutes);
+app.use('/library_managemeny_system/books', bookRoutes);
+app.use('/library_managemeny_system/transactions', transactionRoutes);
 
 
 connectDB();
